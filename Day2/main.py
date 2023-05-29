6a049b4fd97077ae058673f79018b5ce131a5365
Yasser Altamimi <altamimiy89@gmail.com>
2023-05-29 13:28:52 +0300

    day 2 part 2 finished

--- Day2/main.py
+++ Day2/main.py
@@ -10,9 +10,9 @@
 # C -> Scissors 
 #
 # You
-# X -> Rock
-# Y -> Paper
-# Z -> Scissors
+# X -> you need to lose
+# Y -> you need to draw
+# Z -> you need to win
 #
 # Total score = sum of scores in each round
 #
@@ -26,52 +26,93 @@
 # 6 if round won
 
 # TODO: get total score of input
-oRock = 'A'
-oPapaer = 'B'
-oScissors = 'C'
+rock = 'A'
+paper = 'B'
+scissors = 'C'
 
-mRock = 'X'
-mPaper = 'Y'
-mScissors = 'Z'
+lose = 'X'
+draw = 'Y'
+win = 'Z'
 
 
-def getWinner(opponent, mine):
-    if mine == mRock:
-        if opponent == oRock:
+def get_round_points(opponent, mine):
+    if mine == rock:
+        if opponent == rock:
             return 3 + 1
-        if opponent == oPapaer:
+        if opponent == paper:
             return 0 + 1
-        if opponent == oScissors:
+        if opponent == scissors:
             return 6 + 1
 
-    elif mine == mPaper:
-        if opponent == oRock:
+    elif mine == paper:
+        if opponent == rock:
             return 6 + 2
-        if opponent == oPapaer:
+        if opponent == paper:
             return 3 + 2
-        if opponent == oScissors:
+        if opponent == scissors:
             return 0 + 2
 
-    elif mine == mScissors:
-        if opponent == oRock:
+    elif mine == scissors:
+        if opponent == rock:
             return 0 + 3
-        if opponent == oPapaer:
+        if opponent == paper:
             return 6 + 3
-        if opponent == oScissors:
+        if opponent == scissors:
             return 3 + 3
     return 0
+
+
+def get_my_choice(opponent, action):
+    if opponent == rock:
+        if action == lose:
+            return scissors
+        if action == draw:
+            return rock
+        if action == win:
+            return paper
+    pass
+
+    if opponent == paper:
+        if action == lose:
+            return rock
+        if action == draw:
+            return paper
+        if action == win:
+            return scissors
+    pass
+
+    if opponent == scissors:
+        if action == lose:
+            return paper
+        if action == draw:
+            return scissors
+        if action == win:
+            return rock
+    pass
 
 with open('input.txt') as f:
 
     result = 0
     testing = '''A Y
-    B X
-    C Z'''
+B X
+C Z'''
+
+    # for line in testing.splitlines():
+    #     print(f"line: {line}")
+    #     opponent_choice = line[0]
+    #     print(f'opponent_choice {opponent_choice}')
+    #     my_needed_action = line[2]
+    #     print(f'action needed {my_needed_action}')
+    #     my_choice = get_my_choice(opponent_choice, my_needed_action)
+    #     print(f'my choice {my_choice}')
+    #     result += getRoundPoints(opponent_choice, my_choice)
+    #     print(f'result {result}')
 
     for line in f:
         opponent_choice = line[0]
-        my_choice = line[2]
-        result += getWinner(opponent_choice, my_choice)
+        my_needed_action = line[2]
+        my_choice = get_my_choice(opponent_choice, my_needed_action)
+        result += get_round_points(opponent_choice, my_choice)
 
     print(result)
 
