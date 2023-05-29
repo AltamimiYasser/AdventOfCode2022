eef0544154b436e8d36639ca772fba74031e8186
Yasser Altamimi <altamimiy89@gmail.com>
2023-05-29 18:08:37 +0300

    day 4 part 1 finished

---
+++ Day4/main.py
@@ -0,0 +1,45 @@
+#
+# 2-4,6-8
+# 2-3,4-5
+# 5-7,7-9
+# 2-8,3-7
+# 6-6,4-6
+# 2-6,4-8
+#
+
+testing = '''2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8'''
+
+with open('input.txt', 'r') as f:
+    result = 0
+
+    # for each line compare each side
+    for line in f:
+        splittedPair = line.split(',')
+
+        first = splittedPair[0].split('-')
+        second = splittedPair[1].split('-')
+        first_min = int(first[0])
+        first_max = int(first[1])
+        second_min = int(second[0])
+        second_max = int(second[1])
+
+
+        if (first_min >= second_min and first_max <= second_max or 
+            second_min >= first_min and second_max <= first_max):
+
+            result += 1
+
+
+    print(result)
+
+# 2-4,6-8
+# 2-3,4-5
+# 5-7,7-9
+# 2-8,3-7
+# 6-6,4-6
+# 2-6,4-8
